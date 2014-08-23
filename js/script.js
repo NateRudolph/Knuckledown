4428a02199276d33cda0f75f45a2d4330923998a
Nate Rudolph <naterudolph@Nates-iMac.local>
2014-08-22 21:12:43 -0400

    Added lots of tasks and styled long list to hide more than a handful

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 $(document).ready(function () {
 
 });
+// IDEA:
+//Starting the timer with a big button. Activates different mode, starts going through the tasks. Smaller button called "kill" or something brings up a menu where you have to type a long motivational sentence about quitting or something. Only then will it kill the time.
