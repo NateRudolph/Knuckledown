e2670e19bb91cbece5eb2ca4b7bd1bc0cc355c82
Nate Rudolph <naterudolph@Nates-MacBook-Pro.local>
2014-08-22 22:01:23 -0400

    Added shadow class for taskList

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-
+    
 });
 // IDEA:
-//Starting the timer with a big button. Activates different mode, starts going through the tasks. Smaller button called "kill" or something brings up a menu where you have to type a long motivational sentence about quitting or something. Only then will it kill the time.
+// Starting the timer with a big button. Activates different mode, starts going through the tasks. 
+// Smaller button called "kill" or something brings up a menu where you have to type a long motivational 
+// sentence about quitting or something. Only then will it kill the time.
