007f009eb3db41a947f9736107799dc252154d79
Nate Rudolph <naterudolph@Nates-MacBook-Pro.local>
2014-08-23 00:56:20 -0400

    Working towards retriving balue from form on submission

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 $(document).ready(function () {
     loadTitle();
+    
     $("#taskList").scroll(function(){
         $(this).addClass("innerShadow");
         setTimeout(function(){
             $("#taskList").removeClass("innerShadow");
         },1000);
     });
+    
+    $("#taskInput").keyup(function(){
+        alert("asdf");
+    });
+    
 });
 // IDEA:
 // Starting the timer with a big button. Activates different mode, starts going through the tasks. 
 // Smaller button called "kill" or something brings up a menu where you have to type a long motivational 
 // sentence about quitting or something. Only then will it kill the time.
-////////////
+////////////////////////////////////////////////////////////
 function loadTitle(){
     var title = $("h1");
     title.css({"opacity":0,"bottom":-10,"position":"relative"});
