e2ff3f9c85286de9e7b8d730f41d9cf1829c44a4
Nate Rudolph <naterudolph@Nates-MacBook-Pro.local>
2014-08-22 22:31:43 -0400

    Added intro animation for title

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    loadTitle();
     $("#taskList").scroll(function(){
         $(this).addClass("innerShadow");
         setTimeout(function(){
@@ -10,3 +11,12 @@ $(document).ready(function () {
 // Starting the timer with a big button. Activates different mode, starts going through the tasks. 
 // Smaller button called "kill" or something brings up a menu where you have to type a long motivational 
 // sentence about quitting or something. Only then will it kill the time.
+////////////
+function loadTitle(){
+    var title = $("h1");
+    title.css({"opacity":0,"bottom":-10,"position":"relative"});
+    title.animate({
+        "opacity":1,
+        "bottom":"10"
+    },1500);
+}
