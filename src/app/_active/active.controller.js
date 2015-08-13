641c20db622751d9bd2295a9edb3af59d02cc783
NateRudolph <naterudolph@gmail.com>
2015-08-13 10:57:38 -0400

    Basic active functionality with dummy data

--- src/app/_active/active.controller.js
+++ src/app/_active/active.controller.js
@@ -1,6 +1,27 @@
 'use strict';
 
 angular.module('knuckledown')
-  .controller('ActiveCtrl', function ($scope) {
-    
+  .controller('ActiveCtrl', function ($scope, $timeout, $state) {
+  		// get local storage and start running
+  		// for now, faking it
+  		var list = [{"name":"thing 1", "time":3000},{"name":"thing 2", "time":2000},{"name":"thing 3", "time":5000}],
+  			currentIndex = 0;
+
+  		$scope.runTask = function () {
+  			console.log("Running Tasks");
+			if(currentIndex<list.length){
+				$scope.currentTask = list[currentIndex];
+				$timeout(function () { 
+					console.log("increasing index");
+					currentIndex++; 
+					$scope.runTask();
+				}, list[currentIndex].time);
+			} else {
+				$state.go("list");
+			}
+  		};
+
+  		$scope.runTask();
+
+
   });
