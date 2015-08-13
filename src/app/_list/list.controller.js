bb9459ae358d9fc3acb97130c5e4b11b0eaac0da
NateRudolph <naterudolph@gmail.com>
2015-08-13 10:34:50 -0400

    Create and delete functioning for list view

--- src/app/_list/list.controller.js
+++ src/app/_list/list.controller.js
@@ -3,4 +3,16 @@
 angular.module('knuckledown')
   .controller('ListCtrl', function ($scope) {
     
+    var list = [];
+
+  	$scope.addTask = function (taskName, taskTime) {
+  		list.push({ name: taskName, time: taskTime });
+  		$scope.list = list;
+  	};
+
+  	$scope.deleteTask = function (x) {
+  		list.splice(x,1);
+  		$scope.list = list;
+  	};
+
   });
